31ea18eff206f9993fd2be086b32641a900e1734
LeyPro <leypro1502@gmail.com>
2026-05-04 14:50:59 +0000

    Visual edits

--- src/pages/Pastores.jsx
+++ src/pages/Pastores.jsx
@@ -72,7 +72,7 @@ export default function Pastores() {
               {/* Photo */}
               <div className={`${i % 2 === 1 ? 'md:col-start-2' : ''}`}>
                 <div className="aspect-[4/5] rounded-2xl overflow-hidden">
-                  <img src="https://media.base44.com/images/public/69ea6ca06c4574e55a2de28d/34fd18254_PASTORAWEB.jpg" alt={pastor.name} className="w-full h-full object-cover" />
+                  <img src="https://media.base44.com/images/public/69ea6ca06c4574e55a2de28d/896529113_PASTORMAURICIO6.jpg" alt={pastor.name} className="w-full h-full object-cover" />
                 </div>
               </div>
               {/* Info */}
