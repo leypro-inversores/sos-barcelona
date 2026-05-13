6231041bf68ab758a3c33523b7d609c98478f079
LeyPro <leypro1502@gmail.com>
2026-05-13 08:04:59 +0000

    Visual edits

--- src/components/landing/MessageSection.jsx
+++ src/components/landing/MessageSection.jsx
@@ -19,11 +19,11 @@ export default function MessageSection() {
           Una vida con Dios<br />lo cambia todo
         </motion.h2>
 
-        <motion.div
-          initial={{ opacity: 0, scaleX: 0 }}
-          animate={isInView ? { opacity: 1, scaleX: 1 } : {}}
-          transition={{ duration: 0.7, delay: 0.25 }}
-          className="w-12 h-0.5 bg-primary mx-auto mb-10 origin-center hidden" />
+        
+
+
+
+        
         
 
         <motion.p
