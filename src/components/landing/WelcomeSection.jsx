5f4ca9475827cd95e9d6f6a70a3c95ade6316175
LeyPro <leypro1502@gmail.com>
2026-05-19 18:54:30 +0000

    Visual edits

--- src/components/landing/WelcomeSection.jsx
+++ src/components/landing/WelcomeSection.jsx
@@ -51,7 +51,7 @@ export default function WelcomeSection() {
           <img src="https://media.base44.com/images/public/69ea6ca06c4574e55a2de28d/e0f58a8e2_NUEVA_PORTADA.png"
 
           alt="Familia SOS Barcelona"
-          className="w-full h-auto object-contain rounded-2xl px-2 py-2 mx-1" />
+          className="w-full h-auto object-contain rounded-2xl px-2 py-2 mx-6" />
           
         </motion.div>
       </div>
